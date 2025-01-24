e1d1543087fc08f0693e4e337dd8580f2e25d9a8
Jyothika-github <jyothika3005@gmail.com>
2025-01-24 13:16:03 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,1 +1,6 @@
-
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello Jyothika...");
+        System.out.println("All the best for your Java Learing....!!");
+    }
+}
